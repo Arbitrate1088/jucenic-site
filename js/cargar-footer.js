5ee9d0c3d659af00b696459136885f071b9368a0
Arbitrate1088 <geluisz@hotmail.com>
2025-06-29 18:06:51 -0500

    Actualizo style.css con diseño completo y responsivo

--- js/cargar-footer.js
+++ js/cargar-footer.js
@@ -1,1 +1,9 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', () => {
+  fetch('componentes/footer.html')
+    .then(res => res.text())
+    .then(data => {
+      const footerContainer = document.createElement('div');
+      footerContainer.innerHTML = data;
+      document.body.appendChild(footerContainer);
+    });
+});
