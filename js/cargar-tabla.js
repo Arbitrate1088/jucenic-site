22ed74f4f8f6941e39fb4068c58fb88692700515
Arbitrate1088 <geluisz@hotmail.com>
2025-07-27 23:43:03 -0500

    se modifico resumen.html,creo cargar-tabla.js.

--- js/cargar-tabla.js
+++ js/cargar-tabla.js
@@ -1,1 +1,11 @@
-﻿
+﻿// Este script carga la tabla HTML desde componentes/tabla-resumen.html
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("componentes/tabla-resumen.html")
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("contenedor-tabla").innerHTML = html;
+    })
+    .catch(error => {
+      console.error("Error al cargar la tabla:", error);
+    });
+});
